c31eca23498e5999dd9a0fd8a46ec25e1ac29043
Kieran-Antonio <arul.kieran@gmail.com>
2021-12-06 15:15:07 +0000

    added rod cutting

---
+++ src/com/patterns/dynamicProgramming/easy/python/rod_cutting.py
@@ -0,0 +1,79 @@
+# --------------------------------- TOP DOWN DP ---------------------------------
+
+def rod_cutting_top_down_dp(price, length, max_profits_map, cuts_table):
+
+    if length == 0:
+
+        return 0, cuts_table
+
+    for i in range(1, length + 1):
+
+        price_if_cut_at_length_i = price[i] + rod_cutting_top_down_dp(price, length - i, max_profits_map, cuts_table)[0]
+
+        if price_if_cut_at_length_i > max_profits_map[length]:
+
+            max_profits_map[length] = price_if_cut_at_length_i
+            cuts_table[length] = i
+
+    return max_profits_map[length], cuts_table
+
+
+def top_down_driver_function(price, length):
+
+    max_profits_map = {}
+    cuts_table = [0 for _ in range(length + 1)]
+
+    for i in range(length + 1):
+
+        max_profits_map[i] = -1
+
+    max_profit, cuts = rod_cutting_top_down_dp(price, length, max_profits_map, cuts_table)
+
+    n = length
+    answer_table = []
+
+    while n > 0:
+
+        answer_table.append(cuts_table[n])
+        n -= cuts_table[n]
+
+    return answer_table, max_profit
+
+
+# --------------------------------- BOTTOM UP DP ---------------------------------
+
+
+def rod_cutting_bottom_up_dp(price, length):
+
+    max_profit_table = [0 for _ in range(length + 1)]
+    cuts_table = [0 for _ in range(length + 1)]
+
+    for i in range(1, length + 1):
+
+        for j in range(1, i + 1):
+
+            price_if_cut_at_length_j_of_rod_with_length_i = price[j] + max_profit_table[i - j]
+
+            if price_if_cut_at_length_j_of_rod_with_length_i > max_profit_table[i]:
+
+                max_profit_table[i] = price_if_cut_at_length_j_of_rod_with_length_i
+                cuts_table[i] = j
+
+    n = length
+    answer_table = []
+
+    while n > 0:
+
+        answer_table.append(cuts_table[n])
+        n -= cuts_table[n]
+
+    return answer_table, max_profit_table[length]
+
+
+# --------------------------------- TEST CODE ---------------------------------
+
+price_arr = [0, 2, 5, 9, 10, 12, 13, 15, 16]
+rod_length = 8
+
+print(top_down_driver_function(price_arr, rod_length))
+print(rod_cutting_bottom_up_dp(price_arr, rod_length))
